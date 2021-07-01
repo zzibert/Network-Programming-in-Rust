aab472afbd439042d688b5737c7de09bca848b7a
Žan Žibert <zzibert@plume.com>
2021-07-01 12:42:06 +0200

    Hello rust

---
+++ hello-rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+/Users/zzibert/Network-Programming-in-Rust/hello-rust/target/debug/deps/hello_rust: src/main.rs
+
+/Users/zzibert/Network-Programming-in-Rust/hello-rust/target/debug/deps/hello_rust.d: src/main.rs
+
+src/main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+/Users/zzibert/Network-Programming-in-Rust/hello-rust/target/debug/hello-rust: /Users/zzibert/Network-Programming-in-Rust/hello-rust/src/main.rs
