5b67f5ec87c05e40db5e2c40073e5400493f8fa2
Žan Žibert <zzibert@plume.com>
2021-07-01 12:53:57 +0200

    hello world with colors

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,3 +1,12 @@
+extern crate term;
+
 fn main() {
-    println!("Hello, world!");
+    let mut t = term::stdout().unwrap();
+    t.fg(term::color::GREEN).unwrap();
+    write!(t, "hello, ").unwrap();
+
+    t.fg(term::color::RED).unwrap();
+    writeln!(t, "world!").unwrap();
+
+    t.reset().unwrap();
 }
